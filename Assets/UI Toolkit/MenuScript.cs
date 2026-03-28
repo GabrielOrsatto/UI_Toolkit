66a56485105917219c3d8aca077a5c50bd4d5a6b
GABRIEL PRADEGAN ORSATTO <1135097@ATITUS.LOCAL>
2026-03-27 21:48:42 -0300

    Menu Inicial

---
+++ Assets/UI Toolkit/MenuScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UIToolkitDemo
+{
+    public class MenuScript : MonoBehaviour
+    {
+        void Awake()
+        {
+            
+        }
+
+        void Start()
+        {
+            
+        }
+
+    }
+}
